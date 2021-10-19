8f93af242e5e03cd79a95976c37f97149b865ef3
gregk27 <git@gregk.ca>
2021-10-19 10:38:43 -0400

    WH-10; Create class for reminder data model

---
+++ src/models/Reminder.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Greg on 2021-10-19.
+//
+
+#include "Reminder.h"
+using namespace models;
+
+uuid Reminder::getId() {
+    return id;
+}
+
+uuid Reminder::getEventId() {
+    return eventId;
+}
---
+++ src/models/Reminder.h
@@ -0,0 +1,35 @@
+//
+// Created by Greg on 2021-10-19.
+//
+
+#ifndef TASQ_APPLICATION_REMINDER_H
+#define TASQ_APPLICATION_REMINDER_H
+
+#include "ModelBase.h"
+
+/**
+ * Classes and functions for handling data models
+ */
+namespace models {
+    /**
+     * Data model representing a reminder for the local user
+     */
+    class Reminder {
+    private:
+        uuid id;
+        uuid eventId;
+    public:
+        /**
+         * Get the reminder's id
+         */
+        uuid getId();
+
+        /**
+         * Get the reminder's event's id
+         */
+        uuid getEventId();
+    };
+}
+
+
+#endif //TASQ_APPLICATION_REMINDER_H
