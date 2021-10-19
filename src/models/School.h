8d5211f2e2391f614b2ba8dd733dbf7f3a60edbc
gregk27 <git@gregk.ca>
2021-10-19 00:59:27 -0400

    WH-10; Create basic School model

---
+++ src/models/School.h
@@ -0,0 +1,29 @@
+//
+// Created by Greg on 2021-10-19.
+//
+
+#ifndef TASQ_APPLICATION_SCHOOL_H
+#define TASQ_APPLICATION_SCHOOL_H
+
+#include "ModelBase.h"
+
+/**
+ * Classes and functions for handling data models
+ */
+namespace models {
+    /**
+     * Data model representing a school
+     */
+    class School: public ModelBase {
+    private:
+        uuid id;
+        string name;
+    public:
+        /**
+         * Get the school's name
+         */
+        string getName();
+    };
+}
+
+#endif //TASQ_APPLICATION_SCHOOL_H
