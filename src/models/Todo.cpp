06b0b94a39e91ea87f6e73d0987ba1854db80fef
gregk27 <git@gregk.ca>
2021-10-19 10:47:29 -0400

    WH-10; Create class for to-do model

---
+++ src/models/Todo.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Greg on 2021-10-19.
+//
+
+#include "Todo.h"
+using namespace models;
+
+uuid Todo::getId() {
+    return id;
+}
+
+string Todo::getName() {
+    return name;
+}
+
+string Todo::setName(string &newName) {
+    // TODO: Add database changes
+    name = newName;
+}
+
+bool Todo::getCompleted() {
+    return completed;
+}
+
+void Todo::setCompleted(bool newCompleted) {
+    // TODO: Add database changes
+    completed = newCompleted;
+}
---
+++ src/models/Todo.h
@@ -0,0 +1,52 @@
+//
+// Created by Greg on 2021-10-19.
+//
+
+#ifndef TASQ_APPLICATION_TODO_H
+#define TASQ_APPLICATION_TODO_H
+
+#include "ModelBase.h"
+
+/**
+ * Classes and functions for handling data models
+ */
+namespace models {
+
+    /**
+     * Data model representing a To-do item for the local user
+     */
+    class Todo : public ModelBase {
+    private:
+        uuid id;
+        string name;
+        bool completed;
+    public:
+        /**
+         * Get the To-do's id
+         */
+        uuid getId();
+
+        /**
+         * Get the To-do's name
+         */
+        string getName();
+
+        /**
+         * Set the To-do's name, will be reflected in databases
+         */
+        string setName(string &newName);
+
+        /**
+         * Get the To-do's completion flag
+         */
+        bool getCompleted();
+
+        /**
+         * Set the To-do's completion flag, will be reflected in databases
+         */
+        void setCompleted(bool newCompleted);
+    };
+}
+
+
+#endif //TASQ_APPLICATION_TODO_H
