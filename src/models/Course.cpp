0b1d30fedc6a6f1aea7ef5295add2aae1b4cb82a
gregk27 <git@gregk.ca>
2021-10-19 01:57:09 -0400

    WH-10; Create class for course data model

---
+++ src/models/Course.cpp
@@ -0,0 +1,68 @@
+//
+// Created by Greg on 2021-10-19.
+//
+
+#include "Course.h"
+using namespace models;
+
+uuid Course::getId() {
+    return id;
+}
+
+string Course::getName() {
+    return name;
+}
+
+void Course::setName(string &newName) {
+    // TODO: Add database changes
+    name = newName;
+}
+
+string Course::getCode() {
+    return code;
+}
+
+void Course::setCode(string &newCode) {
+    // TODO: Add database changes
+    code = newCode;
+}
+
+unsigned short Course::getYear() {
+    return year;
+}
+
+void Course::setYear(unsigned short &newYear) {
+    // TODO: Add database changes
+    year = newYear;
+}
+
+Term Course::getTerm() {
+    return term;
+}
+
+void Course::setTerm(Term &newTerm) {
+    // TODO: Add database changes
+    term = newTerm;
+}
+
+string Course::getProf() {
+    return prof;
+}
+
+void Course::setProf(string &newProf) {
+    // TODO: Add database changes
+    prof = newProf;
+}
+
+uuid Course::getSchoolId() {
+    return schoolId;
+}
+
+void Course::setSchoolId(uuid &newSchoolId) {
+    // TODO: Add database changes
+    schoolId = newSchoolId;
+}
+
+unsigned long long Course::getModified() {
+    return modified;
+}
