862c0881dbbaf70d61766188037e8ab27ae6c8e8
gregk27 <git@gregk.ca>
2021-10-21 10:02:23 -0400

    WH-16; Minor fixes to net::get, added documentation

--- include/net/net.h
+++ include/net/net.h
@@ -8,15 +8,33 @@
 #include <string>
 #include <curl/curl.h>
 
+/**
+ * Namespace containing netcode for interfacing with backend API
+ */
 namespace net {
+    /** Curl handle used for networking */
     extern CURL *curl;
 
+    /** Base URL for API endpoints */
     const std::string BASE_URL = "https://tasq.gregk.ca";
 
+    /**
+     * Initialise the netcode.<br/>
+     * Must be called before any other operations can be performed
+     */
     void init();
 
+    /**
+     * Execute and HTTP or HTTPS GET request
+     * @param url The URL to request
+     * @return string with response
+     */
     std::string get(std::string url);
 
+    /**
+     * Get the API status
+     * @returns True if API is alive and can be reached
+     */
     bool getStatus();
 }
 
--- src/net/net.cpp
+++ src/net/net.cpp
@@ -8,15 +8,22 @@
 using namespace std;
 using namespace net;
 
+/** Struct to store curl response data */
 typedef struct {
+    /** Memory containing response text */
     char *memory;
+    /** Size of response text */
     size_t size;
 } MemoryStruct;
 
+/**
+ * Callback used by curl to write memory into a MemoryStruct <br/>
+ * Taken from https://curl.se/libcurl/c/getinmemory.html
+ */
 static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     size_t realsize = size * nmemb;
-    MemoryStruct *mem = (MemoryStruct *)userp;
+    auto *mem = (MemoryStruct *)userp;
 
     char *ptr = (char *)realloc(mem->memory, mem->size + realsize + 1);
     if(!ptr) {
@@ -33,16 +40,23 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
     return realsize;
 }
 
+/** Initialise curl handle to nullptr */
 CURL *net::curl = nullptr;
 
 void net::init() {
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
 
+    // Set memory callback
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    // Set SSL CA certificates
+    curl_easy_setopt (curl, CURLOPT_CAINFO, "../lib/bin/cacert.pem");
+    // Specify user agent
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 }
 
 string net::get(string url){
+    // Create a memory chunk to use
     MemoryStruct chunk;
     chunk.memory = (char*)malloc(1);
     chunk.size = 0;
@@ -51,10 +65,6 @@ string net::get(string url){
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     // Specify chunk for callback
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
-    // Specify user agent
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-    // Set SSL CA certificates
-    curl_easy_setopt (curl, CURLOPT_CAINFO, "../lib/bin/cacert.pem");
 
     CURLcode res = curl_easy_perform(curl);
 
@@ -63,7 +73,13 @@ string net::get(string url){
         cerr << curl_easy_strerror(res) << endl;
         return "";
     }
-    return string(chunk.memory);
+
+    // Create string from response
+    string out(chunk.memory);
+    // Free allocated memory to prevent leak
+    free(chunk.memory);
+
+    return out;
 }
 
 bool net::getStatus() {
