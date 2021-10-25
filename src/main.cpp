ab5c76df9bd089e3fccf7a638bd8256a8e01dc13
gregk27 <git@gregk.ca>
2021-10-24 20:07:29 -0400

    WH-26; Update main function to sign in to a dummy user account, removed outdated tests

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include <nlohmann/json.hpp>
 
 #include <models/School.h>
+#include <models/User.h>
+#include <net/auth.h>
 
 using namespace std;
 
@@ -16,29 +18,16 @@ int main(int argc, char *argv[]) {
     QPushButton button("Hello world!", &window);
     window.show();
 
-//    net::init();
-//
-//    cout << net::getStatus() << endl;
-
-    json j = {
-            {"id","16c6bddb-2de2-11ec-a3fd-0023aea14009"},
-            {"name","Software Dev"},
-            {"code","cmpe320"},
-            {"year",2021},
-            {"term","Fall"},
-            {"prof","El Mezouar"},
-            {"owner","71e04118-2dd7-11ec-a3fd-0023aea14009"},
-            {"school","5cd83d02-2dcf-11ec-a3fd-0023aea14009"},
-            {"modified", 1635044502}
-    };
-
-    string str = R"({"id": "asdf", "name": "Queen's"})";
-
-    auto js = nlohmann::json::parse(str);
-
-    models::School s(js);
-
-    cout << s.getName() << endl;
+    net::init();
+    bool netStat = net::getStatus();
+    if(netStat) {
+        string email = "dummy@gregk.ca";
+        string password = "password";
+        net::auth::login(email, password);
+        cout << "Logged in as " << net::auth::localUser->getName() << endl;
+    } else {
+        cout << "API connection failed" << endl;
+    }
 
     return QApplication::exec();
 }
