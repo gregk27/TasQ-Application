34a77eac9332800446a6fd9cdb0a0542cfbbb8b3
gregk27 <git@gregk.ca>
2021-10-26 18:08:20 -0400

    WH-24; Update Todo model to use NetModel

--- src/models/Todo.cpp
+++ src/models/Todo.cpp
@@ -3,7 +3,13 @@
 //
 
 #include <models/Todo.h>
+#include <models/User.h>
 using namespace models;
+
+// Forward declare localUser since cannot import auth
+namespace net::auth {
+    extern shared_ptr<User> localUser;
+}
 
 Todo::Todo(json &json) {
     id = json["id"];
@@ -31,4 +37,23 @@ bool Todo::getCompleted() {
 void Todo::setCompleted(bool newCompleted) {
     // TODO: Add database changes
     completed = newCompleted;
+}
+
+string Todo::getURL(Action a) {
+    switch(a){
+        case NetModel::ADD:
+            return "/users/"+net::auth::localUser->getId()+"/todos/add";
+        case NetModel::MODIFY:
+            return "/users/"+net::auth::localUser->getId()+"/todos/"+id+"/modify";
+        case NetModel::REMOVE:
+            return "/users/"+net::auth::localUser->getId()+"/todos/"+id+"/remove";
+    }
+    throw ActionException("none", "todo");
+}
+
+map<string, string> *Todo::getBody(Action a) {
+    return new map<string, string>{
+            {"name", name},
+            {"completed", to_string(completed)}
+    };
 }
