7c43899aabc7171af2edea9ec5381575c8c42afa
Ethan Orlander <ethanorlander@gmail.com>
2021-10-31 23:04:43 -0400

    feat: add tab navigation

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow window(nullptr);
-    QPushButton button("Hello world!", &window);
     window.show();
     return QApplication::exec();
 }
--- src/widgets/MainWindow/MainWindow.cpp
+++ src/widgets/MainWindow/MainWindow.cpp
@@ -8,12 +8,14 @@
 #include "ui_MainWindow.h"
 
 
-MainWindow::MainWindow(QWidget *parent) :
-        QMainWindow(parent), ui(new Ui::MainWindow) {
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow() {
+MainWindow::~MainWindow()
+{
     delete ui;
 }
-
