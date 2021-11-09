e21455d1d9f3213273b7c40fc697889be0ccd995
Kyle549 <17hl66@queensu.ca>
2021-11-09 18:20:51 -0500

    to do

---
+++ src/widgets/MainWindow/MainWindow.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Greg on 2021-10-14.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
+
+#include <widgets/MainWindow.h>
+#include "ui_MainWindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+        QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
