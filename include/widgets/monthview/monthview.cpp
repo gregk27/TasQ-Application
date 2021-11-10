1954a7311828fc12a30e4defdcdb01f5bb2b7452
18akp9 <85652406+18akp9@users.noreply.github.com>
2021-11-10 00:01:15 -0500

    Added widget for creating a monthly calendar. Added widget for a monthly assignment calendar. Added widget for a term assignment calendar.

---
+++ include/widgets/monthview/monthview.cpp
@@ -0,0 +1,32 @@
+#include "monthview.h"
+#include "ui_monthview.h"
+
+MonthView::MonthView(QWidget* parent) :
+    QWidget(parent),
+    ui(new Ui::MonthView)
+{
+    ui->setupUi(this);
+
+    // Create a single month calendar
+    MonthCalendar* cal = new MonthCalendar(QDate::currentDate().month());
+
+    /*
+    *  Example for adding assignments
+    *  Will change when connected to database
+    */
+    QDate* duedate = new QDate(2021, 11, 21);
+    QTime* duetime = new QTime(19, 0, 0, 0);
+    QDateTime* due = new QDateTime;
+    due->setDate(*duedate);
+    due->setTime(*duetime);
+    cal->AddTAssign("ABCD 123", "Assignment 1", "Assignment", due, "pink");
+
+    QVBoxLayout* lay = new QVBoxLayout;
+    lay->addWidget(cal);
+    setLayout(lay);
+}
+
+MonthView::~MonthView()
+{
+    delete ui;
+}
