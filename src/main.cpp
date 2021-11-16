4eb2bb92130aef4f02e3dfa1099f7d2e34a12f8c
sidbruno123 <57552039+sidbruno123@users.noreply.github.com>
2021-11-16 16:40:21 -0500

    WH-28; Updated ProfileScreen

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow window(nullptr);
-    QPushButton button("Hello world!", &window);
+    //QPushButton button("Hello world!", &window); - delete
     window.show();
     return QApplication::exec();
 }
