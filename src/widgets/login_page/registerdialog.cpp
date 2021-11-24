d8b895eecc2095cd9ca88769eb46b95688429a42
gregk27 <git@gregk.ca>
2021-11-24 00:19:41 -0500

    WH-86; Connect login and registration dialogs to backend

--- src/widgets/login_page/registerdialog.cpp
+++ src/widgets/login_page/registerdialog.cpp
@@ -1,11 +1,15 @@
 #include <widgets/registerdialog.h>
+#include <net/auth.h>
 #include "ui_registerdialog.h"
+#include <widgets/login.h>
 
 RegisterDialog::RegisterDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::RegisterDialog)
 {
     ui->setupUi(this);
+    setModal(true);
+    connect(this, &RegisterDialog::registerUser, AuthController::instance(), &AuthController::registerSlot);
 }
 
 RegisterDialog::~RegisterDialog()
@@ -15,15 +19,14 @@ RegisterDialog::~RegisterDialog()
 
 void RegisterDialog::on_ok_clicked()
 {
-    /*  Need to include data model
+    QString username = ui->l1->text();
+    QString password = ui->l2->text();
+    QString email = ui->l3->text();
+    QString school = ui->l4->text();
 
-      username = ui->l1->text().toStdString();
-    password = ui->l2->text().toStdString();
-     email = ui->l3->text().toStdString();
-     school = ui->l4->text().toStdString();
-
-     */
+    emit registerUser(username, email, password, school);
     close();
+    parentWidget()->close();
 }
 
 
