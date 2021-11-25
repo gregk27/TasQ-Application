023c4178df6f1e652aaa09d5dbaca9269f7d3737
gregk27 <git@gregk.ca>
2021-11-25 02:25:26 -0500

    WH-86; Overhaul addRemoveClass dialog, add space to search for a class

--- src/widgets/AddRemoveClass/addremoveclass.cpp
+++ src/widgets/AddRemoveClass/addremoveclass.cpp
@@ -9,6 +9,10 @@
 
 #include <models/Course.h>
 #include <utils.h>
+#include <net/auth.h>
+#include <net/schools.h>
+#include <net/endpoints.h>
+#include <QPushButton>
 #include <ApplicationController.h>
 
 AddRemoveClass::AddRemoveClass(QWidget *parent) :
@@ -18,9 +22,13 @@ AddRemoveClass::AddRemoveClass(QWidget *parent) :
     setModal(true);
     coursesChanged();
 
-    connect(ui->addButton, SIGNAL(clicked()), this, SIGNAL(addCourse()));
+    QString schoolId = AuthController::instance()->getLocalUser()->getSchoolId();
+    courses = net::schools::getCourses(schoolId);
+
+//    connect(ui->addButton, SIGNAL(clicked()), this, SIGNAL(addCourse()));
     connect(this, &AddRemoveClass::removeCourse, ApplicationController::instance(), &ApplicationController::unsubscribe);
     connect(ApplicationController::instance(), &ApplicationController::coursesChanged, this, &AddRemoveClass::coursesChanged);
+    showSearchResults("");
 }
 
 AddRemoveClass::~AddRemoveClass() {
@@ -36,30 +44,72 @@ void AddRemoveClass::populateClasses(std::unordered_map<QString, models::Course*
         layout = new QVBoxLayout(this);
         ui->classes->setLayout(layout);
     }
-    for(auto [cId, course] : courses){
-        auto *c = new QFrame();
-
-        auto *l = new QLabel(c);
-        l->setText(course->getCode());
-        l->setGeometry(QRect(0, 0, 60, 20));
-
-        l = new QLabel(c);
-        l->setText(course->getName());
-        l->setGeometry(QRect(60, 0, 250, 20));
-
-        auto *btn = new QPushButton(c);
+    for(auto [cId, c] : courses){
+        QPushButton *btn;
+        layout->addWidget(buildFrameForCourse(c, &btn));
         btn->setText("Remove");
-        btn->setGeometry(QRect(300, 0, 80, 20));
-        c->setMinimumHeight(20);
-        // Emit remove course signal when the button is pressed
-        connect(btn, &QPushButton::clicked, [&, course=course]  {
+        // Emit remove c signal when the button is pressed
+        connect(btn, &QPushButton::clicked, [&, course=c]  {
             emit removeCourse(course);
         });
-
-        layout->addWidget(c);
     }
 
     layout->addItem(new QSpacerItem(1, 1000));
+}
+
+void AddRemoveClass::showSearchResults(QString search){
+    utils::clearLayout(ui->searchResults->layout());
+    for(auto c : *courses){
+        if(search == "" || c.getCode().toLower().startsWith(search.toLower()) || c.getName().toLower().startsWith(search.toLower())){
+            // TODO: Hide if already subscribed
+            QPushButton *btn;
+            ui->searchResults->layout()->addWidget(buildFrameForCourse(&c, &btn));
+            btn->setText("Subscribe");
+        }
+    }
+    ui->searchResults->layout()->addItem(new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding));
+}
+
+QFrame *AddRemoveClass::buildFrameForCourse(Course *c, QPushButton **btn){
+    auto *frame = new QFrame();
+    auto layout = new QHBoxLayout();
+    frame->setLayout(layout);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(25);
+
+    auto *l = new QLabel(frame);
+    l->setText(c->getCode());
+    l->setFixedWidth(60);
+    l->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
+    layout->addWidget(l);
+
+    l = new QLabel(frame);
+    l->setText(c->getName());
+    layout->addWidget(l);
+    l->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    l = new QLabel(frame);
+    l->setText(c->getTerm().toString() + " " + QString::number(c->getYear()));
+    layout->addWidget(l);
+    l->setFixedWidth(70);
+    l->setAlignment(Qt::AlignmentFlag::AlignRight);
+    l->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
+
+    l = new QLabel(frame);
+    // Todo optimize?
+    auto oId = c->getOwnerId();
+    l->setText(net::getUser(oId)->getName());
+    layout->addWidget(l);
+    l->setFixedWidth(100);
+    l->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Expanding);
+
+    *btn = new QPushButton(frame);
+    (*btn)->setText("BUTTON");
+    (*btn)->setGeometry(QRect(300, 0, 80, 20));
+    frame->setMinimumHeight(20);
+    layout->addWidget(*btn);
+
+    return frame;
 }
 
 void AddRemoveClass::coursesChanged() {
