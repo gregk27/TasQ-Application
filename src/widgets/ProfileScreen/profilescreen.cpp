3039ea8f9b0b0872d03a202986c6e430ca9439e6
gregk27 <git@gregk.ca>
2021-11-27 02:56:54 -0500

    WH-87; Merge AddRemoveClass into profile page

--- src/widgets/ProfileScreen/profilescreen.cpp
+++ src/widgets/ProfileScreen/profilescreen.cpp
@@ -13,7 +13,6 @@ ProfileScreen::ProfileScreen(QWidget *parent) :
 {
     ui->setupUi(this);
     connect(AuthController::instance(), &AuthController::authStateChanged, this, &ProfileScreen::onAuthStateChanged);
-    connect(ApplicationController::instance(), &ApplicationController::coursesChanged, this, &ProfileScreen::onCoursesChanged);
 }
 
 ProfileScreen::~ProfileScreen()
@@ -26,23 +25,4 @@ void ProfileScreen::onAuthStateChanged(models::User* localUser) {
         ui->label_2->setText("");
     else
         ui->label_2->setText(localUser->getName());
-}
-
-void ProfileScreen::onCoursesChanged(){
-    auto layout = (QVBoxLayout*) ui->classes->layout();
-    utils::clearLayout(layout);
-
-    auto courses = ApplicationController::instance()->getInstances<models::Course>();
-    for(auto [cId, c] : courses){
-        auto widget = new QCheckBox(ui->classes);
-        widget->setChecked(true);
-        widget->setText(c->getCode() + " - " + c->getName());
-        layout->addWidget(widget);
-    }
-    layout->addItem(new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding));
-}
-
-void ProfileScreen::onAddRemoveButtonPressed() {
-    AddRemoveClass *arc = new AddRemoveClass(this);
-    arc->show();
 }
