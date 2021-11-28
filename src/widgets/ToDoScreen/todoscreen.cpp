11643706e438902e9a7d97556d7c0a1fe272791a
gregk27 <git@gregk.ca>
2021-11-28 02:12:25 -0500

    WH-86; Redesign ToDo screen and connect to backend

--- src/widgets/ToDoScreen/todoscreen.cpp
+++ src/widgets/ToDoScreen/todoscreen.cpp
@@ -1,11 +1,14 @@
 #include <widgets/todoscreen.h>
 #include "./ui_todoscreen.h"
+#include <ApplicationController.h>
 
 ToDoScreen::ToDoScreen(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::ToDoScreen)
 {
     ui->setupUi(this);
+
+    connect(ApplicationController::instance(), &ApplicationController::todosChanged, this, &ToDoScreen::update);
 }
 
 ToDoScreen::~ToDoScreen()
@@ -13,45 +16,52 @@ ToDoScreen::~ToDoScreen()
     delete ui;
 }
 
-
-void ToDoScreen::on_p1_clicked()
-{
-    ui->t1->setText("");
-    ui->c1->setChecked(false);
+void ToDoScreen::createTodo() {
+    if(ui->todoInput->text().isEmpty()) return;
+    QJsonObject js = {
+            {"id", ""},
+            {"name", ui->todoInput->text()},
+            {"completed", false}
+    };
+    ApplicationController::instance()->addInstance<Todo>(new Todo(js));
 }
 
-
-void ToDoScreen::on_p2_clicked()
-{
-    ui->t2->setText("");
-    ui->c2->setChecked(false);
+void ToDoScreen::update(){
+    auto todos = ApplicationController::instance()->getInstances<Todo>();
+    ui->activeTodos->clearContent();
+    ui->completedTodos->clearContent();
+    for(auto [tId, t] : todos){
+        if(t->getCompleted()){
+            ui->completedTodos->addWidget(createTodoWidget(t));
+        } else {
+            ui->activeTodos->addWidget(createTodoWidget(t));
+        }
+    }
 }
 
+QWidget *ToDoScreen::createTodoWidget(models::Todo *t) {
+    auto widget = new QWidget();
+    widget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    widget->setLayout(new QHBoxLayout());
 
-void ToDoScreen::on_p3_clicked()
-{
-    ui->t3->setText("");
-    ui->c3->setChecked(false);
+    auto l = new QLabel(t->getName());
+    widget->layout()->addWidget(l);
+
+    auto btn = new QPushButton();
+    btn->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    widget->layout()->addWidget(btn);
+    if(t->getCompleted()){
+        l->setEnabled(false);
+        btn->setText("Remove");
+        connect(btn, &QPushButton::clicked, [=]{
+            ApplicationController::instance()->removeInstance<Todo>(t);
+        });
+    } else {
+        btn->setText("Complete");
+        connect(btn, &QPushButton::clicked, [=]{
+            t->setCompleted(true);
+            ApplicationController::instance()->modifyInstance<Todo>(t);
+        });
+    }
+    return widget;
 }
-
-
-void ToDoScreen::on_p4_clicked()
-{
-    ui->t4->setText("");
-    ui->c4->setChecked(false);
-}
-
-
-void ToDoScreen::on_p5_clicked()
-{
-    ui->t5->setText("");
-    ui->c5->setChecked(false);
-}
-
-
-void ToDoScreen::on_p6_clicked()
-{
-    ui->t6->setText("");
-    ui->c6->setChecked(false);
-}
-
