af37044960cf0aaeeb303a64eae940760dc69d9d
gregk27 <git@gregk.ca>
2021-11-28 00:17:56 -0500

    WH-87; Create dedicated scrollwidget

---
+++ include/widgets/scrollwidget.h
@@ -0,0 +1,32 @@
+//
+// Created by Greg on 2021-11-27.
+//
+
+#ifndef TASQ_APPLICATION_SCROLLWIDGET_H
+#define TASQ_APPLICATION_SCROLLWIDGET_H
+
+#include <QScrollArea>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class ScrollWidget; }
+QT_END_NAMESPACE
+
+class ScrollWidget : public QScrollArea {
+    Q_OBJECT
+
+public:
+    explicit ScrollWidget(QWidget *parent = nullptr);
+
+    ~ScrollWidget() override;
+
+public slots:
+    void clearContent();
+    void addWidget(QWidget *widget);
+
+private:
+    Ui::ScrollWidget *ui;
+};
+
+
+#endif //TASQ_APPLICATION_SCROLLWIDGET_H
